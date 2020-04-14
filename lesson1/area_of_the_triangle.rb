c778a796c6dda0691c4f4eec313dd8db2874b336
5anchezzz <5an4e3.free@gmail.com>
2020-04-14 14:11:31 +0300

    fixed comments

--- lesson1/area_of_the_triangle.rb
+++ lesson1/area_of_the_triangle.rb
@@ -1,7 +1,7 @@
-puts "Назовите высоту треугольника"
-height = gets.chomp
+puts 'Назовите высоту треугольника'
+height = gets.chomp.to_i
 
-puts "Назовите основание треугольника"
-base = gets.chomp
+puts 'Назовите основание треугольника'
+base = gets.chomp.to_i
 
-puts "Площадь треугольника составляент #{0.5 * height.to_i * base.to_i}"
+puts "Площадь треугольника составляент #{0.5 * height * base}"
