d41ec3bdc57dcdab12d384ebc0bd680f9c27120f
5anchezzz <5an4e3.free@gmail.com>
2020-04-14 13:16:24 +0300

    add value of d

--- lesson1/quadratic_equation.rb
+++ lesson1/quadratic_equation.rb
@@ -14,9 +14,11 @@ c = gets.chomp.to_i
 d = b**2 - 4 * a * c
 
 if d>0
+  puts "D = #{d}"
   puts "x1 = #{ (-b + Math.sqrt(d)) / (2 * a) }"
   puts "x2 = #{ (-b - Math.sqrt(d)) / (2 * a) }"
 elsif d == 0
+  puts "D = 0"
   puts "x1,2 = #{ -b / (2 * a) }"
 else
   puts "Корней нет"
