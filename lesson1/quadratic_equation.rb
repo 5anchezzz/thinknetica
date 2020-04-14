b28f01bcb2b4e377ac86b48adc97e9644beb42e8
5anchezzz <5an4e3.free@gmail.com>
2020-04-14 13:59:56 +0300

    add spaces

--- lesson1/quadratic_equation.rb
+++ lesson1/quadratic_equation.rb
@@ -13,7 +13,7 @@ c = gets.chomp.to_i
 
 d = b**2 - 4 * a * c
 
-if d>0
+if d > 0
   puts "D = #{d}"
   puts "x1 = #{ (-b + Math.sqrt(d)) / (2 * a) }"
   puts "x2 = #{ (-b - Math.sqrt(d)) / (2 * a) }"
